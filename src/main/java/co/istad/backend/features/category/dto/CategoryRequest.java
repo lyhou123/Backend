3732ba40fe2256777ea96c4a9aebb2efbe1bc712
Lyhou Phiv <lyhou282@gmail.com>
2025-02-27 12:33:39 +0700

    fix validation

--- src/main/java/co/istad/backend/features/category/dto/CategoryRequest.java
+++ src/main/java/co/istad/backend/features/category/dto/CategoryRequest.java
@@ -1,13 +1,12 @@
 package co.istad.backend.features.category.dto;
 
-import jakarta.validation.constraints.Max;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record CategoryRequest(
         @NotBlank(message = "Name is required")
-        @Max(value = 255, message = "Name is too long")
         String name
 ) {
 }
